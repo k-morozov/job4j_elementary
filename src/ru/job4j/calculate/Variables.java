508b1fde38dfde10d63636fc8227f79c1d582298
Konstantin Morozov <konstantin.morozov@openmedia.co>
2021-03-07 20:18:34 +0300

    0. Что такое переменная. [#488833]

---
+++ src/ru/job4j/calculate/Variables.java
@@ -0,0 +1,9 @@
+package ru.job4j.calculate;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 18;
+        float length = (float)78.0332;
+        boolean baby = true;
+    }
+}
